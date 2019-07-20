6c4c546e14d79135c65182a305d5c645ba5c6995
Epimodev <godin.fred8@gmail.com>
2019-07-20 18:08:17 +0200

    Create test for min and max operators

---
+++ src/operators/max.test.ts
@@ -0,0 +1,77 @@
+import subscribe from '../utils/subscribe';
+import max from './max';
+import { intervalValues } from '../test/callbags';
+
+describe('operators/max', () => {
+  test('should receive only 1 value', done => {
+    const values = [5, 8, 3, 6, 2];
+    const source = intervalValues({ values, duration: 50 });
+    const transformedSource = max()(source);
+    const next = jest.fn();
+
+    subscribe(transformedSource)({ next });
+
+    setTimeout(() => {
+      expect(next).toBeCalledTimes(1);
+
+      done();
+    }, 300);
+  });
+
+  test('should complete at the end of the source', done => {
+    const values = [5, 8, 3, 6, 2];
+    const source = intervalValues({ values, duration: 50 });
+    const transformedSource = max()(source);
+    const complete = jest.fn();
+
+    subscribe(transformedSource)({ complete });
+
+    setTimeout(() => {
+      expect(complete).toBeCalledTimes(1);
+
+      done();
+    }, 300);
+  });
+
+  test('should failed if source fail', done => {
+    const values = [5, 8, 3, 6, 2];
+    const source = intervalValues({ values, duration: 50, willFail: true });
+    const transformedSource = max()(source);
+    const error = jest.fn();
+
+    subscribe(transformedSource)({ error });
+
+    setTimeout(() => {
+      expect(error).toBeCalledTimes(1);
+
+      done();
+    }, 100);
+  });
+
+  test('should cancel source on unsubscribe', () => {
+    const cancelMock = jest.fn();
+    const values = [5, 8, 3, 6, 2];
+    const source = intervalValues({ values, duration: 50, cancelMock });
+    const transformedSource = max()(source);
+
+    const unsubscribe = subscribe(transformedSource)({});
+    unsubscribe();
+
+    expect(cancelMock).toBeCalledTimes(1);
+  });
+
+  test('should send max value from source', done => {
+    const values = [5, 8, 3, 6, 2];
+    const source = intervalValues({ values, duration: 50 });
+    const transformedSource = max()(source);
+    const next = jest.fn();
+
+    subscribe(transformedSource)({ next });
+
+    setTimeout(() => {
+      expect(next).lastCalledWith(8);
+
+      done();
+    }, 300);
+  });
+});
