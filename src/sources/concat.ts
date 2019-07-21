6f3e6c954acf3fc4347eb7db17b2249bb1b0ec37
Epimodev <godin.fred8@gmail.com>
2019-07-21 16:16:27 +0200

    Refacto concat and merge source with mergePool

--- src/sources/concat.ts
+++ src/sources/concat.ts
@@ -1,12 +1,5 @@
-import {
-  CALLBAG_START,
-  CALLBAG_RECEIVE,
-  CALLBAG_FINISHING,
-  CallbagType,
-  Callbag,
-  Source,
-  Sink,
-} from '../index';
+import { Source } from '../index';
+import mergePool from './mergePool';
 
 function concat<T1>(s1: Source<T1>): Source<T1>;
 function concat<T1, T2>(s1: Source<T1>, s2: Source<T2>): Source<T1 | T2>;
@@ -77,64 +70,7 @@ function concat<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
 function concat<T = any>(...sources: Source<T>[]): Source<T>;
 
 function concat<T = any>(...sources: Source<T>[]): Source<T> {
-  // @ts-ignore
-  return (start: CallbagType, sink: Sink<T>) => {
-    if (start === CALLBAG_START) {
-      const nbSources = sources.length;
-      let nbFinished = 0;
-      let started = false;
-      let finished = false;
-      const sourceTalkbacks = new Array<Callbag<any, any>>(nbSources);
-
-      const talkback = (type: CallbagType, payload: any) => {
-        if (type === CALLBAG_FINISHING) {
-          finished = true;
-        }
-        for (let i = 0; i < nbSources; i += 1) {
-          sourceTalkbacks[i] && sourceTalkbacks[i](type as any, payload);
-        }
-      };
-
-      const startSource = (index: number) => {
-        sources[index](CALLBAG_START, (type: CallbagType, payload: any) => {
-          switch (type) {
-            case CALLBAG_START:
-              sourceTalkbacks[index] = payload;
-              if (!started) {
-                started = true;
-                sink(CALLBAG_START, talkback);
-              }
-              break;
-            case CALLBAG_RECEIVE:
-              sink(type, payload);
-              break;
-            case CALLBAG_FINISHING:
-              nbFinished += 1;
-
-              if (payload) {
-                // if there is an error
-                finished = true;
-
-                sink(CALLBAG_FINISHING, payload);
-              } else {
-                // if callbag complete
-                finished = finished || nbFinished === nbSources;
-
-                if (finished) {
-                  sink(CALLBAG_FINISHING);
-                } else {
-                  // nbFinished is equals to next index source to start
-                  startSource(nbFinished);
-                }
-              }
-              break;
-          }
-        });
-      };
-
-      startSource(0);
-    }
-  };
+  return mergePool(sources, 1);
 }
 
 export default concat;
--- src/sources/merge.ts
+++ src/sources/merge.ts
@@ -1,12 +1,5 @@
-import {
-  CALLBAG_START,
-  CALLBAG_RECEIVE,
-  CALLBAG_FINISHING,
-  CallbagType,
-  Callbag,
-  Source,
-  Sink,
-} from '../index';
+import { Source } from '../index';
+import mergePool from './mergePool';
 
 function merge<T1>(s1: Source<T1>): Source<T1>;
 function merge<T1, T2>(s1: Source<T1>, s2: Source<T2>): Source<T1 | T2>;
@@ -77,65 +70,7 @@ function merge<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
 function merge<T = any>(...sources: Source<T>[]): Source<T>;
 
 function merge<T = any>(...sources: Source<T>[]): Source<T> {
-  // @ts-ignore
-  return (start: CallbagType, sink: Sink<any>) => {
-    if (start === CALLBAG_START) {
-      const nbSources = sources.length;
-      let nbFinished = 0;
-      let started = false;
-      let finished = false;
-      const sourceTalkbacks = new Array<Callbag<any, any>>(nbSources);
-
-      const talkback = (type: CallbagType, payload: any) => {
-        if (type === CALLBAG_FINISHING) {
-          finished = true;
-        }
-        for (let i = 0; i < nbSources; i += 1) {
-          sourceTalkbacks[i] && sourceTalkbacks[i](type as any, payload);
-        }
-      };
-
-      for (let i = 0; i < nbSources; i += 1) {
-        sources[i](CALLBAG_START, (type: CallbagType, payload: any) => {
-          switch (type) {
-            case CALLBAG_START:
-              sourceTalkbacks[i] = payload;
-              if (!started) {
-                started = true;
-                sink(CALLBAG_START, talkback);
-              }
-              break;
-            case CALLBAG_RECEIVE:
-              sink(type, payload);
-              break;
-            case CALLBAG_FINISHING:
-              nbFinished += 1;
-
-              if (payload) {
-                // if there is an error
-                finished = true;
-
-                for (let j = 0; j < nbSources; j += 1) {
-                  if (j !== i) {
-                    // if callbag fail, we cancel other sources
-                    sourceTalkbacks[j] && sourceTalkbacks[j](CALLBAG_FINISHING);
-                  }
-                }
-                sink(CALLBAG_FINISHING, payload);
-              } else {
-                // if callbag complete
-                finished = finished || nbFinished === nbSources;
-
-                if (finished) {
-                  sink(CALLBAG_FINISHING);
-                }
-              }
-              break;
-          }
-        });
-      }
-    }
-  };
+  return mergePool(sources, Infinity);
 }
 
 export default merge;
