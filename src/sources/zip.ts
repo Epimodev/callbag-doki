aa5823a4c135b4750cd146db806ab354bd5af1fb
Epimodev <godin.fred8@gmail.com>
2019-08-08 23:00:42 +0200

    Refacto zip source

--- src/sources/zip.ts
+++ src/sources/zip.ts
@@ -1,12 +1,6 @@
-import {
-  CALLBAG_START,
-  CALLBAG_RECEIVE,
-  CALLBAG_FINISHING,
-  CallbagType,
-  Callbag,
-  Source,
-  Sink,
-} from '../index';
+import { Source } from '../index';
+import subscribe from '../utils/subscribe';
+import { createSource } from './';
 
 function zip<T1>(sources: [Source<T1>], poolSize?: number): Source<[T1]>;
 function zip<T1, T2>(sources: [Source<T1>, Source<T2>], poolSize?: number): Source<[T1, T2]>;
@@ -75,82 +69,69 @@ function zip<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(
 function zip<T = any>(sources: Source<T>[], poolSize?: number): Source<T[]>;
 
 function zip<T = any>(sources: Source<T>[], poolSize = Infinity): Source<T[]> {
-  // @ts-ignore
-  return (start: CallbagType, sink: Sink<T[]>) => {
-    if (start === CALLBAG_START) {
-      const nbSources = sources.length;
-      let nbStarted = 0;
-      let nbFinished = 0;
-      let started = false;
-      let finished = false;
-      const batches: T[][] = [];
-      const sourceTalkbacks = new Array<Callbag<any, any>>(nbSources);
+  return createSource((next, complete, error) => {
+    const nbSources = sources.length;
+    let nbStarted = 0;
+    let nbFinished = 0;
+    let finished = false;
+    const batches: T[][] = [];
+    const unsubscribeFuncs = new Array<() => void>(nbSources);
 
-      const talkback = (type: CallbagType, payload: any) => {
-        if (type === CALLBAG_FINISHING) {
-          finished = true;
-        }
-        for (let i = 0; i < nbSources; i += 1) {
-          sourceTalkbacks[i] && sourceTalkbacks[i](type as any, payload);
-        }
-      };
+    const handleComplete = () => {
+      nbFinished += 1;
+      finished = finished || nbFinished === nbSources;
 
-      const startSource = (index: number) => {
-        nbStarted += 1;
-        sources[index](CALLBAG_START, (type: CallbagType, payload: any) => {
-          switch (type) {
-            case CALLBAG_START:
-              sourceTalkbacks[index] = payload;
-              if (!started) {
-                started = true;
-                sink(CALLBAG_START, talkback);
-              }
-              break;
-            case CALLBAG_RECEIVE:
-              appendValue(batches, payload, index, nbSources);
-              if (isBatchComplete(batches[0])) {
-                // if batch is complete, we get it from batches and send it
-                const batch = batches.shift();
-                sink(type, batch!);
-              }
-              break;
-            case CALLBAG_FINISHING:
-              nbFinished += 1;
-
-              if (payload) {
-                // if there is an error
-                finished = true;
-
-                for (let j = 0; j < nbSources; j += 1) {
-                  if (j !== index) {
-                    // if callbag fail, we cancel other sources
-                    sourceTalkbacks[j] && sourceTalkbacks[j](CALLBAG_FINISHING);
-                  }
-                }
-                sink(CALLBAG_FINISHING, payload);
-              } else {
-                // if callbag complete
-                finished = finished || nbFinished === nbSources;
-
-                if (finished) {
-                  sink(CALLBAG_FINISHING);
-                } else if (nbStarted < nbSources) {
-                  // all sources are started but some aren't already finished
-                  // nbStarted is equals to next index source to start
-                  startSource(nbStarted);
-                }
-              }
-              break;
-          }
-        });
-      };
-
-      const nbToStart = Math.min(nbSources, poolSize);
-      while (nbStarted < nbToStart) {
+      if (finished) {
+        complete();
+      } else if (nbStarted < nbSources) {
+        // start next source which isn't started yet
         startSource(nbStarted);
       }
+      // all sources are started but some aren't already finished
+    };
+
+    const startSource = (index: number) => {
+      nbStarted += 1;
+
+      const handleNext = (value: T) => {
+        appendValue(batches, value, index, nbSources);
+        if (isBatchComplete(batches[0])) {
+          // if batch is complete, we get it from batches and send it
+          const batch = batches.shift();
+          next(batch!);
+        }
+      };
+
+      const handleError = (err: any) => {
+        finished = true;
+        for (let j = 0; j < nbSources; j += 1) {
+          if (j !== index) {
+            // if callbag fail, we cancel other sources
+            unsubscribeFuncs[j] && unsubscribeFuncs[j]();
+          }
+        }
+        error(err);
+      };
+
+      unsubscribeFuncs[index] = subscribe(sources[index])({
+        next: handleNext,
+        complete: handleComplete,
+        error: handleError,
+      });
+    };
+
+    const nbToStart = Math.min(nbSources, poolSize);
+    while (nbStarted < nbToStart) {
+      startSource(nbStarted);
     }
-  };
+
+    return () => {
+      finished = true;
+      for (let i = 0; i < nbSources; i += 1) {
+        unsubscribeFuncs[i] && unsubscribeFuncs[i]();
+      }
+    };
+  });
 }
 
 /**
