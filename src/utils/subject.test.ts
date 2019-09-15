986e877859e4c36989569dd4de468e6b05ac3cf7
Epimodev <godin.fred8@gmail.com>
2019-09-15 10:58:45 +0200

    Add createSubject utils

--- src/utils/subject.test.ts
+++ src/utils/subject.test.ts
@@ -1,192 +1,303 @@
-import { createMulticastedSource } from './subject';
+import { createSubject, createMulticastedSource } from './subject';
 import { intervalValues } from '../test/callbags';
 
-describe('utils/multi casted source', () => {
-  test('should receive values', done => {
-    const values = [1, 2, 3];
-    const source = intervalValues({ values, duration: 100 });
-    const next = jest.fn();
+describe('utils/subject', () => {
+  describe('create subject', () => {
+    test('should receive values', () => {
+      const next1 = jest.fn();
+      const next2 = jest.fn();
 
-    const subject = createMulticastedSource(source);
+      const subject = createSubject<number>();
+      subject.subscribe({ next: next1 });
+      subject.subscribe({ next: next2 });
 
-    subject.subscribe({ next });
+      subject.next(1);
+      subject.next(2);
+      subject.next(3);
 
-    setTimeout(() => {
-      expect(next).toBeCalledTimes(values.length);
-      expect(next).toHaveBeenNthCalledWith(1, values[0]);
-      expect(next).toHaveBeenNthCalledWith(2, values[1]);
-      expect(next).toHaveBeenNthCalledWith(3, values[2]);
+      expect(next1).toHaveBeenCalledTimes(3);
+      expect(next1).toHaveBeenNthCalledWith(1, 1);
+      expect(next1).toHaveBeenNthCalledWith(2, 2);
+      expect(next1).toHaveBeenNthCalledWith(3, 3);
 
-      done();
-    }, 350);
-  });
+      expect(next2).toHaveBeenCalledTimes(3);
+      expect(next2).toHaveBeenNthCalledWith(1, 1);
+      expect(next2).toHaveBeenNthCalledWith(2, 2);
+      expect(next2).toHaveBeenNthCalledWith(3, 3);
+    });
 
-  test('should receive error', done => {
-    const values = [1, 2, 3];
-    const source = intervalValues({ values, duration: 100, willFail: true });
-    const error = jest.fn();
+    test('should receive error', () => {
+      const error1 = jest.fn();
+      const error2 = jest.fn();
 
-    const subject = createMulticastedSource(source);
+      const subject = createSubject<number>();
+      subject.subscribe({ error: error1 });
+      subject.subscribe({ error: error2 });
 
-    subject.subscribe({ error });
+      subject.error('Error message');
 
-    setTimeout(() => {
-      expect(error).toBeCalledTimes(1);
+      expect(error1).toHaveBeenCalledTimes(1);
+      expect(error2).toHaveBeenCalledTimes(1);
+    });
 
-      done();
-    }, 150);
-  });
+    test('should complete', () => {
+      const complete1 = jest.fn();
+      const complete2 = jest.fn();
 
-  test('should complete', done => {
-    const values = [1, 2, 3];
-    const source = intervalValues({ values, duration: 100 });
-    const complete = jest.fn();
+      const subject = createSubject<number>();
+      subject.subscribe({ complete: complete1 });
+      subject.subscribe({ complete: complete2 });
 
-    const subject = createMulticastedSource(source);
+      subject.complete();
 
-    subject.subscribe({ complete });
+      expect(complete1).toHaveBeenCalledTimes(1);
+      expect(complete2).toHaveBeenCalledTimes(1);
+    });
 
-    setTimeout(() => {
-      expect(complete).toBeCalledTimes(1);
+    test("shouldn't receive value before and after unsubscribe", () => {
+      const next1 = jest.fn();
+      const next2 = jest.fn();
 
-      done();
-    }, 350);
-  });
+      const subject = createSubject<number>();
+      subject.subscribe({ next: next1 });
 
-  test('should not receive any value after unsubscribe', done => {
-    const values = [1, 2, 3];
-    const source = intervalValues({ values, duration: 100 });
-    const next = jest.fn();
-
-    const subject = createMulticastedSource(source);
-
-    const unsubscribe = subject.subscribe({ next });
-
-    setTimeout(() => {
+      subject.next(1);
+      const unsubscribe = subject.subscribe({ next: next2 });
+      subject.next(2);
       unsubscribe();
-      expect(next).toBeCalledTimes(1);
-    }, 150);
+      subject.next(3);
 
-    setTimeout(() => {
-      expect(next).toBeCalledTimes(1);
+      expect(next1).toHaveBeenCalledTimes(3);
+      expect(next1).toHaveBeenNthCalledWith(1, 1);
+      expect(next1).toHaveBeenNthCalledWith(2, 2);
+      expect(next1).toHaveBeenNthCalledWith(3, 3);
 
-      done();
-    }, 350);
+      expect(next2).toHaveBeenCalledTimes(1);
+      expect(next2).toHaveBeenNthCalledWith(1, 2);
+    });
+
+    test("shouldn't receive error before and after unsubscribe", () => {
+      const error1 = jest.fn();
+      const error2 = jest.fn();
+
+      const subject = createSubject<number>();
+      subject.subscribe({ error: error1 });
+
+      subject.error('Error message 1');
+      const unsubscribe = subject.subscribe({ error: error2 });
+      subject.error('Error message 2');
+      unsubscribe();
+      subject.error('Error message 3');
+
+      expect(error1).toHaveBeenCalledTimes(3);
+      expect(error2).toHaveBeenCalledTimes(1);
+    });
+
+    test("shouldn't complete before and after unsubscribe", () => {
+      const complete1 = jest.fn();
+      const complete2 = jest.fn();
+
+      const subject = createSubject<number>();
+      subject.subscribe({ complete: complete1 });
+
+      subject.complete();
+      const unsubscribe = subject.subscribe({ complete: complete2 });
+      subject.complete();
+      unsubscribe();
+      subject.complete();
+
+      expect(complete1).toHaveBeenCalledTimes(3);
+      expect(complete2).toHaveBeenCalledTimes(1);
+    });
   });
 
-  test('both subscriptions shoud receive values', done => {
-    const values = [1, 2, 3];
-    const source = intervalValues({ values, duration: 100 });
-    const next1 = jest.fn();
-    const next2 = jest.fn();
+  describe('multi casted source', () => {
+    test('should receive values', done => {
+      const values = [1, 2, 3];
+      const source = intervalValues({ values, duration: 100 });
+      const next = jest.fn();
 
-    const subject = createMulticastedSource(source);
+      const subject = createMulticastedSource(source);
 
-    subject.subscribe({ next: next1 });
-    subject.subscribe({ next: next2 });
+      subject.subscribe({ next });
 
-    setTimeout(() => {
-      expect(next1).toBeCalledTimes(values.length);
-      expect(next1).toHaveBeenNthCalledWith(1, values[0]);
-      expect(next1).toHaveBeenNthCalledWith(2, values[1]);
-      expect(next1).toHaveBeenNthCalledWith(3, values[2]);
+      setTimeout(() => {
+        expect(next).toBeCalledTimes(values.length);
+        expect(next).toHaveBeenNthCalledWith(1, values[0]);
+        expect(next).toHaveBeenNthCalledWith(2, values[1]);
+        expect(next).toHaveBeenNthCalledWith(3, values[2]);
 
-      expect(next2).toBeCalledTimes(values.length);
-      expect(next2).toHaveBeenNthCalledWith(1, values[0]);
-      expect(next2).toHaveBeenNthCalledWith(2, values[1]);
-      expect(next2).toHaveBeenNthCalledWith(3, values[2]);
+        done();
+      }, 350);
+    });
 
-      done();
-    }, 350);
-  });
+    test('should receive error', done => {
+      const values = [1, 2, 3];
+      const source = intervalValues({ values, duration: 100, willFail: true });
+      const error = jest.fn();
 
-  test('both subscriptions shoud receive error', done => {
-    const values = [1, 2, 3];
-    const source = intervalValues({ values, duration: 100, willFail: true });
-    const error1 = jest.fn();
-    const error2 = jest.fn();
+      const subject = createMulticastedSource(source);
 
-    const subject = createMulticastedSource(source);
+      subject.subscribe({ error });
 
-    subject.subscribe({ error: error1 });
-    subject.subscribe({ error: error2 });
+      setTimeout(() => {
+        expect(error).toBeCalledTimes(1);
 
-    setTimeout(() => {
-      expect(error1).toBeCalledTimes(1);
-      expect(error2).toBeCalledTimes(1);
+        done();
+      }, 150);
+    });
 
-      done();
-    }, 150);
-  });
+    test('should complete', done => {
+      const values = [1, 2, 3];
+      const source = intervalValues({ values, duration: 100 });
+      const complete = jest.fn();
 
-  test('both subscriptions shoud complete', done => {
-    const values = [1, 2, 3];
-    const source = intervalValues({ values, duration: 100 });
-    const complete1 = jest.fn();
-    const complete2 = jest.fn();
+      const subject = createMulticastedSource(source);
 
-    const subject = createMulticastedSource(source);
+      subject.subscribe({ complete });
 
-    subject.subscribe({ complete: complete1 });
-    subject.subscribe({ complete: complete2 });
+      setTimeout(() => {
+        expect(complete).toBeCalledTimes(1);
 
-    setTimeout(() => {
-      expect(complete1).toBeCalledTimes(1);
-      expect(complete2).toBeCalledTimes(1);
+        done();
+      }, 350);
+    });
 
-      done();
-    }, 350);
-  });
+    test("shouldn't receive any value after unsubscribe", done => {
+      const values = [1, 2, 3];
+      const source = intervalValues({ values, duration: 100 });
+      const next = jest.fn();
 
-  test('second subscription should receive value on subscription', done => {
-    const values = [1, 2, 3];
-    const source = intervalValues({ values, duration: 100 });
-    const next1 = jest.fn();
-    const next2 = jest.fn();
+      const subject = createMulticastedSource(source);
 
-    const subject = createMulticastedSource(source);
+      const unsubscribe = subject.subscribe({ next });
 
-    const unsubscribe1 = subject.subscribe({ next: next1 });
+      setTimeout(() => {
+        unsubscribe();
+        expect(next).toBeCalledTimes(1);
+      }, 150);
 
-    setTimeout(() => {
-      const unsubscribe2 = subject.subscribe({ next: next2 });
+      setTimeout(() => {
+        expect(next).toBeCalledTimes(1);
 
-      expect(next1).toBeCalledTimes(2);
-      expect(next1).toHaveBeenNthCalledWith(1, values[0]);
-      expect(next1).toHaveBeenNthCalledWith(2, values[1]);
+        done();
+      }, 350);
+    });
 
-      expect(next2).toBeCalledTimes(1);
-      expect(next2).toHaveBeenNthCalledWith(1, values[1]);
+    test('both subscriptions shoud receive values', done => {
+      const values = [1, 2, 3];
+      const source = intervalValues({ values, duration: 100 });
+      const next1 = jest.fn();
+      const next2 = jest.fn();
 
-      unsubscribe1();
-      unsubscribe2();
+      const subject = createMulticastedSource(source);
 
-      done();
-    }, 250);
-  });
+      subject.subscribe({ next: next1 });
+      subject.subscribe({ next: next2 });
 
-  test('should cancel source only when all subscription are canceled', done => {
-    const values = [1, 2, 3];
-    const cancelMock = jest.fn();
-    const source = intervalValues({ values, duration: 100, cancelMock });
+      setTimeout(() => {
+        expect(next1).toBeCalledTimes(values.length);
+        expect(next1).toHaveBeenNthCalledWith(1, values[0]);
+        expect(next1).toHaveBeenNthCalledWith(2, values[1]);
+        expect(next1).toHaveBeenNthCalledWith(3, values[2]);
 
-    const subject = createMulticastedSource(source);
+        expect(next2).toBeCalledTimes(values.length);
+        expect(next2).toHaveBeenNthCalledWith(1, values[0]);
+        expect(next2).toHaveBeenNthCalledWith(2, values[1]);
+        expect(next2).toHaveBeenNthCalledWith(3, values[2]);
 
-    const unsubscribe1 = subject.subscribe({});
-    const unsubscribe2 = subject.subscribe({});
+        done();
+      }, 350);
+    });
 
-    setTimeout(() => {
-      unsubscribe1();
+    test('both subscriptions shoud receive error', done => {
+      const values = [1, 2, 3];
+      const source = intervalValues({ values, duration: 100, willFail: true });
+      const error1 = jest.fn();
+      const error2 = jest.fn();
 
-      expect(cancelMock).toHaveBeenCalledTimes(0);
-    }, 150);
+      const subject = createMulticastedSource(source);
 
-    setTimeout(() => {
-      unsubscribe2();
+      subject.subscribe({ error: error1 });
+      subject.subscribe({ error: error2 });
 
-      expect(cancelMock).toHaveBeenCalledTimes(1);
+      setTimeout(() => {
+        expect(error1).toBeCalledTimes(1);
+        expect(error2).toBeCalledTimes(1);
 
-      done();
-    }, 250);
+        done();
+      }, 150);
+    });
+
+    test('both subscriptions shoud complete', done => {
+      const values = [1, 2, 3];
+      const source = intervalValues({ values, duration: 100 });
+      const complete1 = jest.fn();
+      const complete2 = jest.fn();
+
+      const subject = createMulticastedSource(source);
+
+      subject.subscribe({ complete: complete1 });
+      subject.subscribe({ complete: complete2 });
+
+      setTimeout(() => {
+        expect(complete1).toBeCalledTimes(1);
+        expect(complete2).toBeCalledTimes(1);
+
+        done();
+      }, 350);
+    });
+
+    test('second subscription should receive value on subscription', done => {
+      const values = [1, 2, 3];
+      const source = intervalValues({ values, duration: 100 });
+      const next1 = jest.fn();
+      const next2 = jest.fn();
+
+      const subject = createMulticastedSource(source);
+
+      const unsubscribe1 = subject.subscribe({ next: next1 });
+
+      setTimeout(() => {
+        const unsubscribe2 = subject.subscribe({ next: next2 });
+
+        expect(next1).toBeCalledTimes(2);
+        expect(next1).toHaveBeenNthCalledWith(1, values[0]);
+        expect(next1).toHaveBeenNthCalledWith(2, values[1]);
+
+        expect(next2).toBeCalledTimes(1);
+        expect(next2).toHaveBeenNthCalledWith(1, values[1]);
+
+        unsubscribe1();
+        unsubscribe2();
+
+        done();
+      }, 250);
+    });
+
+    test('should cancel source only when all subscription are canceled', done => {
+      const values = [1, 2, 3];
+      const cancelMock = jest.fn();
+      const source = intervalValues({ values, duration: 100, cancelMock });
+
+      const subject = createMulticastedSource(source);
+
+      const unsubscribe1 = subject.subscribe({});
+      const unsubscribe2 = subject.subscribe({});
+
+      setTimeout(() => {
+        unsubscribe1();
+
+        expect(cancelMock).toHaveBeenCalledTimes(0);
+      }, 150);
+
+      setTimeout(() => {
+        unsubscribe2();
+
+        expect(cancelMock).toHaveBeenCalledTimes(1);
+
+        done();
+      }, 250);
+    });
   });
 });
--- src/utils/subject.ts
+++ src/utils/subject.ts
@@ -8,6 +8,50 @@ interface Subject<T> {
   complete: () => void;
 }
 
+function createSubject<T>(): Subject<T> {
+  const observers: Observer<T>[] = [];
+
+  const next = (newValue: T) => {
+    for (let i = 0, l = observers.length; i < l; i += 1) {
+      const observer = observers[i];
+      observer.next && observer.next(newValue);
+    }
+  };
+  const error = (err: any) => {
+    for (let i = 0, l = observers.length; i < l; i += 1) {
+      const observer = observers[i];
+      observer.error && observer.error(err);
+    }
+  };
+  const complete = () => {
+    for (let i = 0, l = observers.length; i < l; i += 1) {
+      const observer = observers[i];
+      observer.complete && observer.complete();
+    }
+  };
+
+  const unsubscribeSubject = (observer: Observer<T>) => {
+    const index = observers.indexOf(observer);
+    observers.splice(index, 1);
+  };
+
+  const subscribeSubject = (listener: Observer<T> | Listener<T>): Unsubscribe => {
+    const observer: Observer<T> = typeof listener === 'function' ? { next: listener } : listener;
+    observers.push(observer);
+
+    return () => {
+      unsubscribeSubject(observer);
+    };
+  };
+
+  return {
+    next,
+    error,
+    complete,
+    subscribe: subscribeSubject,
+  };
+}
+
 function createMulticastedSource<T>(source: Source<T>): Subject<T> {
   const observers: Observer<T>[] = [];
   let unsubscribe: Unsubscribe | undefined;
@@ -73,4 +117,4 @@ function createMulticastedSource<T>(source: Source<T>): Subject<T> {
   };
 }
 
-export { createMulticastedSource, Subject };
+export { createSubject, createMulticastedSource, Subject };
